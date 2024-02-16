335e5eb0f6bb28349b9fb3f8b61783000cc30be3
rjspyk5 <rjspyk5@gmail.com>
2024-02-17 02:38:51 +0600

    setup all main functionality,have some calculation related problem

--- js/index.js
+++ js/index.js
@@ -3,12 +3,15 @@ class typingGame{
         this.mainContainer=document.getElementById(main)
         this.play=document.getElementsByClassName(play);
         this.score=document.getElementById("score")
+   
+       this.result=document.getElementById('result');
         this.lifeContainer=document.getElementById("lifeContainer")
         this.lifeField=document.getElementById("life")
-       
+        this.playground=document.getElementById("playground")
         this.charShoingPlace= document.getElementById("place");
         this.keyboard=document.getElementById("keyboard")
-        this.clickSound=new Audio("/audio/mouseCllick.wav");  
+        this.clickSound=new Audio("/audio/mouseCllick.wav");
+        this.gameOver=document.getElementById("gameover")  
         this.mainContainer.addEventListener("click",this.handleClick.bind(this))  
         
     }
@@ -21,37 +24,42 @@ class typingGame{
         }      
     }
     handleKeyboardClick(e){
-        let lifeCount=this.lifeField.innerText
-    if (e.target.innerText===this.charShoingPlace.innerText && lifeCount>0) {
-        let currentScore= parseInt(this.score.innerText);
-        console.log(currentScore);
-        this.score.innerText=currentScore+1;
+        this.lifeCount=this.lifeField.innerText
+    if (e.target.innerText===this.charShoingPlace.innerText && this.lifeCount>0) {
+        this.currentScore= parseInt(this.score.innerText);
+        this.score.innerText=this.currentScore+1;
         this.setRandomLetterOnScreen()   
     }else if(e.target.className.includes("letter")){
-        lifeCount-=1;
-        if (lifeCount==2) {
+         this.lifeCount-=1;
+        if (this.lifeCount==2) {
             this.lifeContainer.style.backgroundColor="salmon"  
         }
-        if (lifeCount==1) {
+        if (this.lifeCount==1) {
             this.lifeContainer.style.backgroundColor="red" 
         }
-        if (lifeCount<1) {
-            console.log("n");
-        }
-        
-        this.lifeField.innerText=lifeCount;
+        if (this.lifeCount<1) {
+            this.gameOverr()
+        } 
+        this.lifeField.innerText=this.lifeCount;
     }
 }
-    targetClickFinder(e){
-        e.target
-
+   gameOverr(){
+       this.playground.classList.add("hidden");
+       this.gameOver.classList.remove("hidden");
+       this.result.innerText=this.currentScore;
+       
     }
+
+
 
     playy(e){
         const removeClassEl=e.target.parentNode.parentNode;
         removeClassEl.classList.add("hidden");
-        const  playground=document.getElementById("playground")
-        playground.classList.remove("hidden")
+        this.playground.classList.remove("hidden")
+        this.lifeField.innerText=3;
+        this.score.innerText=0;
+        this.lifeCount=3;
+        this.currentScore=0;
     }
 
     setRandomLetterOnScreen(){
