10245801a59f8fc622825606f497491b78e3e477
rjspyk5 <rjspyk5@gmail.com>
2024-02-17 13:58:31 +0600

    happened calculation problem because nested eventListner.solved calculation problem

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
 class typingGame{
-    constructor(play,main) {
-        this.mainContainer=document.getElementById(main)
+    constructor(play) {
         this.play=document.getElementsByClassName(play);
-        this.score=document.getElementById("score")
-   
-       this.result=document.getElementById('result');
-        this.lifeContainer=document.getElementById("lifeContainer")
-        this.lifeField=document.getElementById("life")
+        this.score=document.getElementById("score");
+        this.result=document.getElementById('result');
+        this.lifeContainer=document.getElementById("lifeContainer");
+        this.lifeField=document.getElementById("life");  
         this.playground=document.getElementById("playground")
         this.charShoingPlace= document.getElementById("place");
+        this.playAgain=document.getElementById("playAgain")
+        this.play=document.getElementById("play")
         this.keyboard=document.getElementById("keyboard")
         this.clickSound=new Audio("/audio/mouseCllick.wav");
-        this.gameOver=document.getElementById("gameover")  
-        this.mainContainer.addEventListener("click",this.handleClick.bind(this))  
-        
+        this.gameOver=document.getElementById("gameover") 
+        this.currentScore= parseInt(this.score.innerText);
+        this.lifeCount=this.lifeField.innerText; 
+        this.play.addEventListener("click",this.handleClick.bind(this));     
+        this.playAgain.addEventListener("click",this.handleClick.bind(this));  
+        document.addEventListener("keyup",this.handleKeyboardClick.bind(this)) ;   
     }
+ 
     handleClick(e){
-        if (e.target.className.includes("play")) {
-        this.setRandomLetterOnScreen()
-        this.clickSound.play()
-        this.playy(e)
-        this.keyboard.addEventListener("click",this.handleKeyboardClick.bind(this)) 
-        }      
+        this.setRandomLetterOnScreen();
+        this.clickSound.play();
+        this.playy(e);
+        this.reset();
     }
     handleKeyboardClick(e){
-      
-        this.lifeCount=this.lifeField.innerText
-    if (e.target.innerText===this.charShoingPlace.innerText && this.lifeCount>0) {
-        this.currentScore= parseInt(this.score.innerText);
-        this.score.innerText=this.currentScore+1;
-        this.setRandomLetterOnScreen()   
-    }else if(e.target.className.includes("letter")){
-         this.lifeCount-=1;
+    if (e.key===this.charShoingPlace.innerText && this.lifeCount>0) {
+        this.currentScore+=1;
+        this.score.innerText=this.currentScore;
+        this.setRandomLetterOnScreen();
+    }else if(e.key!=this.charShoingPlace && !this.playground.className.includes("hidden")){
+        this.lifeCount=this.lifeCount-1;
+        this.lifeField.innerText=this.lifeCount;
         if (this.lifeCount==2) {
             this.lifeContainer.style.backgroundColor="salmon"  
         }
@@ -41,7 +42,6 @@ class typingGame{
         if (this.lifeCount<1) {
             this.gameOverr()
         } 
-        this.lifeField.innerText=this.lifeCount;
     }
 }
    gameOverr(){
@@ -50,17 +50,20 @@ class typingGame{
        this.result.innerText=this.currentScore;
        
     }
-
+    reset(){
+        this.lifeField.innerText=3;
+        this.score.innerText=0;
+        this.lifeCount=3;
+        this.currentScore=0;
+        this.lifeContainer.style.backgroundColor="#22c55e" 
+    }
 
 
     playy(e){
         const removeClassEl=e.target.parentNode.parentNode;
         removeClassEl.classList.add("hidden");
         this.playground.classList.remove("hidden")
-        this.lifeField.innerText=3;
-        this.score.innerText=0;
-        this.lifeCount=3;
-        this.currentScore=0;
+        
        
     }
 
@@ -76,6 +79,6 @@ class typingGame{
    
 }
 
-const firstVersion=new typingGame("play","main")
+const firstVersion=new typingGame("play")
 
 // const startSound=new Audio("/audio/Games of Thrones __ Ringtone 2015 __ Theme Music(MP3_160K)_1.mp3");
